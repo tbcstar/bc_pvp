03f4407c29ae3b16d99cd6b258669a4eb731f5b3
yuanf225 <admin@itpel.com>
2022-12-06 18:44:28 +0800

    新增中文

---
+++ language/chinese/pvp_lang.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('不允许直接脚本访问');
+
+$lang['pvp_statistics'] = 'PvP统计';
+
+$lang['rank'] = '排名';
+$lang['top_honorable_kills'] = 'Top荣誉击杀';
+$lang['top_arena_teams'] = 'Top竞技场队伍';
+$lang['teams_2v2'] = '2v2 队伍';
+$lang['teams_3v3'] = '3v3 队伍';
+$lang['teams_5v5'] = '5v5 队伍';
+
+$lang['total_kills'] = '总击杀数';
+$lang['today_kills'] = '今日击杀';
+$lang['yersterday_kills'] = '昨日击杀';
+$lang['members'] = '成员';
+$lang['rating'] = '评分';
+$lang['games'] = '比赛';
